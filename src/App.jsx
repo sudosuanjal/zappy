91ff81818d4c20d80f4e2c49d61004eb2670ea52
sudosuanjal <anjalehackchey101@gmail.com>
2025-06-11 01:31:01 +0530

    feat: supabase login and the login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import ChatListSection from "./components/ChatListSection";
 import ActiveChatArea from "./components/ActiveChatArea";
 import { Route, Routes } from "react-router";
 import ChatPage from "./pages/ChatPage";
+import LoginPage from "./pages/LoginPage";
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<ChatPage />} />
-        <Route path="/login" element={<ChatPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </>
   );
