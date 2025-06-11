872af1f87f954838c66062003ffe327edad25940
sudosuanjal <anjalehackchey101@gmail.com>
2025-06-11 17:19:28 +0530

    feat: login route

---
+++ backend/src/controllers/auth/login.controller.js
@@ -0,0 +1,35 @@
+import User from "../../models/user.model.js";
+
+export const login = async (req, res) => {
+  const { email, fullName, username, profilePic } = req.body;
+
+  if (!email || !username || !fullName) {
+    return res.status(400).json({ message: "missing required credentials" });
+  }
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      const newUser = new User({
+        email,
+        fullName,
+        username,
+        profilePic,
+      });
+
+      await newUser.save();
+
+      return res
+        .status(201)
+        .json({ message: "new user created successfully", user: newUser });
+    } else {
+      return res
+        .status(200)
+        .json({ message: "user logged in succesfully", user });
+    }
+  } catch (error) {
+    console.error("login error: " + error);
+    return res.status(500).json({ message: "internal server error" });
+  }
+};
--- backend/src/index.js
+++ backend/src/index.js
@@ -3,10 +3,18 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import { configDotenv } from "dotenv";
 import { connect } from "./database/connectDB.js";
+import authRoutes from "./routes/auth.rotue.js";
 
 configDotenv();
 const PORT = process.env.PORT;
 const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "hello" });
+});
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   connect();
