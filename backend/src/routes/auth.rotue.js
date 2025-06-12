4e6ae80231a7bd3ab8aecdbf3ef0f0f2dd9fc2cb
sudosuanjal <anjalehackchey101@gmail.com>
2025-06-12 12:49:03 +0530

    feat: check auth route

--- backend/src/routes/auth.rotue.js
+++ backend/src/routes/auth.rotue.js
@@ -3,10 +3,12 @@ import { login } from "../controllers/auth/login.controller.js";
 import { verifyAuth } from "../middleware/verify.middleware.js";
 import { ProtectRoute } from "../middleware/protect.middleware.js";
 import { username } from "../controllers/auth/username.controller.js";
+import { checkAuth } from "../controllers/auth/checkAuth.controller.js";
 
 const router = express.Router();
 
 router.post("/login", verifyAuth, login);
 router.put("/username", ProtectRoute, username);
+router.get("/check-auth", ProtectRoute, checkAuth);
 
 export default router;
