44791c97bd1762fed2b8dd5fc929c309b50d378d
sudosuanjal <anjalehackchey101@gmail.com>
2025-06-14 14:24:56 +0530

    feat: both route added to the route

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,11 +1,14 @@
 import express from "express";
 import { getUsers } from "../controllers/message/getUser.controller.js";
 import { ProtectRoute } from "../middleware/protect.middleware.js";
+import { getMessage } from "../controllers/message/getMessage.controller.js";
+import { sendMessage } from "../controllers/message/sendMesssage.controller.js";
 
 const router = express.Router();
 
 router.get("/users", ProtectRoute, getUsers);
+router.get("/:id", ProtectRoute, getMessage);
 
-
+router.post("send/:id", ProtectRoute, sendMessage);
 
 export default router;
